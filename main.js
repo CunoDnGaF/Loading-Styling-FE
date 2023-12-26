2f17ae4c0cfb37e09f0876c3da607c7e773fdaf0
CunoDnGaF <118813084+CunoDnGaF@users.noreply.github.com>
2023-12-26 18:46:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/Widget.js
+class Widget {
+  constructor(container, url) {
+    this.container = container;
+    this.url = url;
+  }
+  async init() {
+    this.registerServiceWorker();
+    await this.newsRendering();
+  }
+  registerServiceWorker() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('./service-worker.js', {
+        scope: './'
+      }).then(req => {
+        console.log('Registration succeeded. Scope is ' + req.scope);
+      }).catch(error => {
+        console.log('Registration failed with error: ' + error);
+      });
+    }
+  }
+  async getNews() {
+    let response;
+    response = await fetch(this.url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return await response.json();
+  }
+  async newsRendering() {
+    let news;
+    let loadingPlug = this.container.querySelector('.loading-plug');
+    let offlineBanner = this.container.querySelector('.offline-banner');
+    try {
+      news = await this.getNews();
+      for (let el of news) {
+        this.renderNews(el);
+      }
+      loadingPlug.classList.add('unactive');
+    } catch (error) {
+      console.log(error);
+      offlineBanner.classList.remove('unactive');
+    }
+  }
+  renderNews(news) {
+    const newsCell = document.createElement('div');
+    this.widgetBody = this.container.querySelector('.widget-body');
+    newsCell.classList.add('news-cell');
+    newsCell.id = news.id;
+    newsCell.innerHTML = `
+      <span class="news-header">${news.heading}</span>
+      <div class="news-body">
+        <img src="${news.image}" class="news-image">
+        <span class="news-text">${news.body}</span>
+      </div>
+      `;
+    this.widgetBody.append(newsCell);
+  }
+}
+;// CONCATENATED MODULE: ./src/js/app.js
+
+const url = 'https://loading-styling-be.onrender.com/';
+const widget = new Widget(document, url);
+widget.init();
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+// TODO: write your code in app.js
+/******/ })()
+;
